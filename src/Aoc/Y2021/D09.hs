48c7c1c573e0bbeaefc7c2df7468dd29233fd5a0
be7a <git@be7a.de>
2021-12-09 17:15:38 +0100

    cleanup smallestNeighbors

--- src/Aoc/Y2021/D09.hs
+++ src/Aoc/Y2021/D09.hs
@@ -20,27 +20,25 @@ parser :: Parser Heightmap
 parser = liftA2 listArray ((,) (1, 1) . liftA2 (,) length (length . head)) concat <$> manyLines (some digit)
 
 smallestNeighbors :: Heightmap -> (Int, Int) -> [(Int, Int)]
-smallestNeighbors heightmap (y, x) = head $ groupBy (\x y -> heightmap ! x == heightmap ! y) $ sortBy compareNeighbors neighbors
+smallestNeighbors heightmap (y, x) = head $ groupBy (\x y -> EQ == compareNeighbors x y) $ sortBy compareNeighbors neighbors
     where
         compareNeighbors x y = (heightmap ! x) `compare` (heightmap ! y)
         neighbors = filter valid $ zipWith (\dy dx -> (y + dy, x + dx)) [0, 0, 0, 1, -1] [0, 1, -1, 0, 0]
         valid = inRange (bounds heightmap)
 
--- Left if c is a lowest point, Right if there is a lower point nearby
-getLowestPoint :: Heightmap -> (Int, Int) -> Either (Int, Int) (Int, Int)
-getLowestPoint heightmap c = if isAbsoluteLowest c lowestNeighbor then Left c else Right (head lowestNeighbor)
-  where
-    lowestNeighbor = smallestNeighbors heightmap c
-    isAbsoluteLowest c = liftA2 (&&) ((==) c . head) ((==) 1 . length)
+isAbsoluteLowest :: (Int, Int) -> [(Int, Int)] -> Bool
+isAbsoluteLowest c = liftA2 (&&) ((==) c . head) ((==) 1 . length)
 
 getLargestBasins :: Heightmap -> [Int]
 getLargestBasins heightmap = sortOn negate basinSize
     where
         getBasin c
             | heightmap ! c == 9 = Nothing
-            | Left d <- lp = Just d
-            | Right d <- lp = basinTable ! d
-            where lp = getLowestPoint heightmap c
+            | isAbsoluteLowest c lpoints = Just smallestNeighbor
+            | otherwise = basinTable ! smallestNeighbor
+            where
+              smallestNeighbor = head lpoints
+              lpoints = smallestNeighbors heightmap c
 
         basinTable = listArray (bounds heightmap) [getBasin c | c <- range $ bounds heightmap]
         basinSize = map length $ group $ sort $ catMaybes $ elems basinTable
@@ -48,7 +46,7 @@ getLargestBasins heightmap = sortOn negate basinSize
 solver :: Heightmap -> IO ()
 solver heightmap = do
   putStrLn ">> Part 1"
-  print $ sum $ map (\x -> (heightmap ! x) + 1) $ filter (isLeft . getLowestPoint heightmap) $ indices heightmap
+  print $ sum $ map (\x -> (heightmap ! x) + 1) $ filter (liftA2 ($) isAbsoluteLowest (smallestNeighbors heightmap)) $ indices heightmap
   putStrLn ">> Part 2"
   print $ product $ take 3 $ getLargestBasins heightmap
 
