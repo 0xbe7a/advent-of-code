f1b38476b2286a5e1ab746354ef3b4d712a9ea38
be7a <git@be7a.de>
2021-12-09 16:33:42 +0100

    mmh yes cryptic code

--- src/Aoc/Y2021/D09.hs
+++ src/Aoc/Y2021/D09.hs
@@ -12,34 +12,35 @@ import Data.Ix
 import Debug.Trace
 import Data.List (sortBy, sort, groupBy, sortOn, group)
 import Data.Maybe (fromMaybe, mapMaybe, isJust, catMaybes)
+import Data.Either (isLeft)
 
 type Heightmap = Array (Int, Int) Integer
 
 parser :: Parser Heightmap
-parser = liftA2 listArray (\x -> ((0, 0), (length x - 1, length (head x) - 1))) concat <$> manyLines (some digit)
+parser = liftA2 listArray ((,) (1, 1) . liftA2 (,) length (length . head)) concat <$> manyLines (some digit)
 
-compareNeighbors :: Heightmap -> (Int, Int) -> (Int, Int) -> Ordering
-compareNeighbors map x y = (map ! x) `compare` (map ! y)
-
-orderNeighbors :: Heightmap -> (Int, Int) -> [(Int, Int)]
-orderNeighbors map (y, x) = sortBy (compareNeighbors map) neighbors
+smallestNeighbors :: Heightmap -> (Int, Int) -> [(Int, Int)]
+smallestNeighbors heightmap (y, x) = head $ groupBy (\x y -> heightmap ! x == heightmap ! y) $ sortBy compareNeighbors neighbors
     where
+        compareNeighbors x y = (heightmap ! x) `compare` (heightmap ! y)
         neighbors = filter valid $ zipWith (\dy dx -> (y + dy, x + dx)) [0, 0, 0, 1, -1] [0, 1, -1, 0, 0]
-        valid = inRange (bounds map)
+        valid = inRange (bounds heightmap)
 
-isLowPoint :: Heightmap -> (Int, Int) -> Bool
-isLowPoint map c = isAbsoluteLowest && c == fst
-    where
-         [fst, snd] = take 2 $ orderNeighbors map c
-         isAbsoluteLowest = LT == compareNeighbors map fst snd
+-- Left if c is a lowest point, Right if there is a lower point nearby
+getLowestPoint :: Heightmap -> (Int, Int) -> Either (Int, Int) (Int, Int)
+getLowestPoint heightmap c = if isAbsoluteLowest c lowestNeighbor then Left c else Right (head lowestNeighbor)
+  where
+    lowestNeighbor = smallestNeighbors heightmap c
+    isAbsoluteLowest c = liftA2 (&&) ((==) c . head) ((==) 1 . length)
 
 getLargestBasins :: Heightmap -> [Int]
 getLargestBasins heightmap = sortOn negate basinSize
     where
         getBasin c
             | heightmap ! c == 9 = Nothing
-            | isLowPoint heightmap c = Just c
-            | otherwise = basinTable ! head (orderNeighbors heightmap c)
+            | Left d <- lp = Just d
+            | Right d <- lp = basinTable ! d
+            where lp = getLowestPoint heightmap c
 
         basinTable = listArray (bounds heightmap) [getBasin c | c <- range $ bounds heightmap]
         basinSize = map length $ group $ sort $ catMaybes $ elems basinTable
@@ -47,7 +48,7 @@ getLargestBasins heightmap = sortOn negate basinSize
 solver :: Heightmap -> IO ()
 solver heightmap = do
   putStrLn ">> Part 1"
-  print $ sum $ map (\x -> (heightmap ! x) + 1) $ liftA2 filter isLowPoint indices heightmap
+  print $ sum $ map (\x -> (heightmap ! x) + 1) $ filter (isLeft . getLowestPoint heightmap) $ indices heightmap
   putStrLn ">> Part 2"
   print $ product $ take 3 $ getLargestBasins heightmap
 
