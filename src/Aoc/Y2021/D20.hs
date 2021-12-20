93f8dcf7d3efd4280669a552238a2ef7a15d15c2
be7a <git@be7a.de>
2021-12-20 16:36:13 +0100

    use set instead of map

--- src/Aoc/Y2021/D20.hs
+++ src/Aoc/Y2021/D20.hs
@@ -5,27 +5,27 @@ module Aoc.Y2021.D20
   )
 where
 
-import Aoc.Day ( dayParse, Day )
-import Aoc.Parse ( char, newline, sepEndBy1, some, (<|>), Parser )
+import Aoc.Day (Day, dayParse)
+import Aoc.Parse (Parser, char, newline, sepEndBy1, some, (<|>))
 import Control.Applicative (Applicative (liftA2))
-import Data.Bits ( Bits((.|.), shiftL) )
+import Data.Bits (Bits (shiftL, (.|.)))
 import Data.List (sort)
-import qualified Data.Map as Map
 import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Set ( Set, member, fromAscList, elems, fromDistinctAscList )
 import GHC.Arr (Array, Ix (inRange, range), bounds, listArray, (!))
 
 type PatternArray = Array Int Bool
 
-type Image = (Bool, Map.Map (Int, Int) Bool)
+type Image = (Bool, Set (Int, Int))
 
 type Input = (PatternArray, Image)
 
 parser :: Parser Input
-parser = (,) <$> (parsePattern <* newline) <*> parseImage
+parser = (,) <$> (parsePattern <* newline) <*> ((False,) . fromAscList . map fst . filter snd <$> parseImage)
   where
     parsePixel = (False <$ char '.') <|> (True <$ char '#')
     parsePattern = listArray (0, 2 ^ 9 - 1) <$> (some parsePixel <* newline)
-    parseImage = (False,) . Map.fromAscList . liftA2 zip (range . (,) (1, 1) . liftA2 (,) length (length . head)) concat <$> (some parsePixel `sepEndBy1` newline)
+    parseImage = liftA2 zip (range . (,) (1, 1) . liftA2 (,) length (length . head)) concat <$> (some parsePixel `sepEndBy1` newline)
 
 dedupOrd :: Eq a => [a] -> [a]
 dedupOrd [] = []
@@ -35,17 +35,19 @@ dedupOrd (x : xs) = dedupOrd' x xs
     dedupOrd' x (y : ys) | x == y = dedupOrd' x ys
     dedupOrd' x (y : ys) = x : dedupOrd' y ys
 
+neighbors :: Num a => (a, a) -> [(a, a)]
+neighbors (y, x) = zipWith (\dy dx -> (y + dy, x + dx)) [-1, -1, -1, 0, 0, 0, 1, 1, 1] (cycle [-1, 0, 1])
+
 applyPattern :: PatternArray -> Image -> Image
 applyPattern pattern (outOfRange, image) = (newOutOfRange, newImage)
   where
     newOutOfRange = if outOfRange then pattern ! (2 ^ 9 - 1) else pattern ! 0
-    neighbors (y, x) = zipWith (\dy dx -> (y + dy, x + dx)) [-1, -1, -1, 0, 0, 0, 1, 1, 1] (cycle [-1, 0, 1])
-    getNeighborPixels = map (\n -> Map.findWithDefault outOfRange n image) . neighbors
+    getNeighborPixels = map (\n -> if member n image then not outOfRange else outOfRange) . neighbors
     getPattern = foldl (\x p -> shiftL x 1 .|. if p then 1 else 0) (0 :: Int) . getNeighborPixels
     kernel c = pattern ! getPattern c
 
-    canidates = dedupOrd . sort . concat $ [k : n | k <- Map.keys image, let n = neighbors k]
-    newImage = Map.fromAscList $ filter (\x -> snd x /= newOutOfRange) $ map (\i -> (i, kernel i)) canidates
+    canidates = dedupOrd . sort . concat $ [k : n | k <- elems image, let n = neighbors k]
+    newImage = fromDistinctAscList $ filter (\i -> kernel i /= newOutOfRange) canidates
 
 solver :: Input -> IO ()
 solver (pattern, image) = do
@@ -54,7 +56,8 @@ solver (pattern, image) = do
   putStrLn ">> Part 2"
   print $ pixelsAfterNstep 50
   where
-    pixelsAfterNstep n = length $ snd $ iterate (applyPattern pattern) image !! n
+    pixelsAfterNstep 0 = image
+    pixelsAfterNstep n = applyPattern pattern $ pixelsAfterNstep (n-1)
 
 day :: Day
 day = dayParse parser solver
